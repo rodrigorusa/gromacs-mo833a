de34615bff0789ff954303af3a8de70cdb9c51cc
Rodrigo Rusa <rodrigorusa@gmail.com>
2020-04-30 18:12:36 -0300

    MO833: Atividade 5

---
+++ experiments/ativ-5-exp-1/build.sh
@@ -0,0 +1,8 @@
+# Compiling
+echo "Compiling..."
+cd ../../
+[ ! -d "build/" ] && mkdir -p "build/"
+cd build
+cmake .. -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON -DGMX_MPI=ON -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx
+make
+make check
