3525a0adb70e14c12980d8d41dfcc10d41eccc56
Rodrigo Rusa <rodrigorusa@gmail.com>
2020-05-16 10:24:29 -0300

    Ajuste no script build.sh

--- experiments/ativ-7-exp-1/build.sh
+++ experiments/ativ-7-exp-1/build.sh
@@ -4,4 +4,4 @@ cd ../../
 [ ! -d "build/" ] && mkdir -p "build/"
 cd build
 cmake .. -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON -DGMX_MPI=ON -DGMX_GPU=OFF -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -DBUILD_SHARED_LIBS=OFF
-make -j3
+make -j
