92b0afe0d44ac0f1301b63e58500162974512e85
Rodrigo Rusa <rodrigorusa@gmail.com>
2020-05-17 17:41:22 -0300

    Adicionando -j4 na compilação

--- experiments/ativ-7-exp-1/build.sh
+++ experiments/ativ-7-exp-1/build.sh
@@ -3,5 +3,5 @@ echo "Compiling..."
 cd ../../
 [ ! -d "build/" ] && mkdir -p "build/"
 cd build
-cmake .. -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON -DGMX_MPI=ON -DGMX_GPU=OFF -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -DBUILD_SHARED_LIBS=OFF
-make -j
+cmake .. -DGMX_BUILD_OWN_FFTW=ON -DGMX_MPI=ON -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx
+make -j4
