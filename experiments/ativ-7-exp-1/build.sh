10408a97ffa9a26b902e965ed50b644ead7a140e
Rodrigo Rusa <rodrigorusa@gmail.com>
2020-05-19 17:40:07 -0300

    Compilando com linkagem estática

--- experiments/ativ-7-exp-1/build.sh
+++ experiments/ativ-7-exp-1/build.sh
@@ -3,5 +3,5 @@ echo "Compiling..."
 cd ../../
 [ ! -d "build/" ] && mkdir -p "build/"
 cd build
-cmake .. -DGMX_BUILD_OWN_FFTW=ON -DGMX_MPI=ON -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx
+cmake .. -DGMX_BUILD_OWN_FFTW=ON -DGMX_MPI=ON -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -DBUILD_SHARED_LIBS=OFF
 make -j4
