23d8c927aa16b762c9fe5ab4735dc69cdb603754
Rodrigo Rusa <rodrigorusa@gmail.com>
2020-05-21 09:19:55 -0300

    Removendo parâmetro -j do make

--- experiments/ativ-7-exp-1/build.sh
+++ experiments/ativ-7-exp-1/build.sh
@@ -4,4 +4,4 @@ cd ../../
 [ ! -d "build/" ] && mkdir -p "build/"
 cd build
 cmake .. -DGMX_BUILD_OWN_FFTW=ON -DGMX_MPI=ON -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -DBUILD_SHARED_LIBS=OFF
-make -j4
+make
